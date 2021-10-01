d02b681c151574b60d185697244a8453f14d976d
Aryan Sonone <75265917+Coding-Aryan@users.noreply.github.com>
2021-10-01 06:38:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ def calc(args):
 
         return args.x * args.y
 
-    elif args.o == 'sub':
+args.o == 'sub':
 
         return args.x - args.y
 
@@ -22,24 +22,4 @@ def calc(args):
 
     else:
 
-        return "Something went wrong"
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--x', type=float, default=1.0,
-
-                        help="Enter first number. This is a utility for calculation. Please contact harry bhai")
-
-    parser.add_argument('--y', type=float, default=3.0,
-
-                        help="Enter second number. This is a utility for calculation. Please contact harry bhai")
-
-    parser.add_argument('--o', type=str, default="add",
-
-                        help="This is a utility for calculation. Please contact harry bhai for more")
-
-    args = parser.parse_args()
-
-    sys.stdout.write(str(calc(args)))
+        return "
